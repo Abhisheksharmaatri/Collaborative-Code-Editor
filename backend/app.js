2a5f8e9dc9720fcfdd600b645aca57a20cb104db
Abhisheksharmaatri <abhishek.21b0311082@abes.ac.in>
2025-01-28 11:12:46 +0530

    solved socket errors

--- backend/app.js
+++ backend/app.js
@@ -49,24 +49,6 @@ app.use('/room', roomCommentRoutes)
 app.use('/code', codeRoutes)
 
 app.use(error)
-// mongoose
-//   .connect(process.env.DB_URL)
-//   .then(result => {
-//     console.log('Connected to database');
-//     server.listen(process.env.PORT, () => {
-//       console.log(`Server is running on port ${process.env.PORT}`);
-//     });
-//   })
-//   .catch(err => console.log(err));
-// mongoose
-//   .connect('mongodb+srv://alexwolfdog:alexwolfdog@code-collabortator.licgrid.mongodb.net/?retryWrites=true&w=majority&appName=Code-Collabortator')
-//   .then(result => {
-//     console.log('Connected to database');
-//     server.listen(4000, () => {
-//       console.log(`Server is running on port 4000`);
-//     });
-//   })
-//   .catch(err => console.log(err));
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
